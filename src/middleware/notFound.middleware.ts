fa33e9379d22263d81cb9cba188f6840c2b1ed84
RomainLefranc <lefranc46@gmail.com>
2023-11-29 00:32:55 +0400

    fix route not found middlware

--- src/middleware/notFound.middleware.ts
+++ src/middleware/notFound.middleware.ts
@@ -1,6 +1,10 @@
-import { NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 import { HttpException } from "@exceptions/HttpException.js";
 
-export const NotFoundMiddleware = (next: NextFunction) => {
+export const NotFoundMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   next(new HttpException(404, "Not Found"));
 };
